9c07b2c41bee52068f583a2bf741058a9dbf2e1e
rahulbotmaster1 <rahulbotmaster00@gmail.com>
2026-02-15 00:50:01 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -9,11 +9,11 @@
 const config = require('../config');
 const { cmd, commands } = require('../command');
 const { proto, downloadContentFromMessage } = require('@whiskeysockets/baileys');
-const { sms,downloadMediaMessage } = require('../Lib/msg');
+const { sms,downloadMediaMessage } = require('../lib/msg');
 const fs = require('fs');
 const exec = require('child_process');
 const path = require('path');
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../Lib/functions');
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../lib/functions');
 const ownerNumber = [config.OWNER_NUMBER];
 const prefix = config.PREFIX;
 //--------------------------------------------
